602d3d8fb96e4af2ebd4bcfa157497db6141f4d0
rhullathy <XMIRQH0@DTW126577N01.clients.nasa.cpwr.corp>
2014-06-20 13:52:45 -0400

    updated

--- Testgit/src/com/git/Test.java
+++ Testgit/src/com/git/Test.java
@@ -7,7 +7,7 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Welcome to git world");
+System.out.println("Welcome to git world and committed now");
 	}
 
 }
